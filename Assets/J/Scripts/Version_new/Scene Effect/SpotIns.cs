bd6fe0bff02d61bff1b9d97ab0eec6c8c6a1f305
Zilong <Zilong@Apple-MacBook-Air.local>
2017-06-02 16:20:46 +0800

    新增移動軌跡跟背景星星

--- Assets/J/Scripts/Version_new/Scene Effect/SpotIns.cs
+++ Assets/J/Scripts/Version_new/Scene Effect/SpotIns.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpotIns : MonoBehaviour {
-	
+	public Transform SpotPosition;
 	public float SpotSpeed;
 
+	private float offsetPos;
+
+	private string Name="光點";
 	private float randonScale;
 	private float randonTime;
-
-
+	private bool canSpawn=true;
 	// Use this for initialization
 	void Start () {
+		randonTime=Random.Range(1,1.5f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(canSpawn)
+			StartCoroutine(Spawn());
+	}
+	IEnumerator Spawn(){
+		canSpawn=false;
+		offsetPos=Random.Range(-2.8f,2.8f);
+		Vector3 pos=SpotPosition.position;
+		pos.x+=offsetPos;
+		GameObject tempSpot;
+		tempSpot=JObjectPool._InstanceJObjectPool.GetGameObject(Name,pos);
+		tempSpot.GetComponent<SpotFly>().speed=SpotSpeed;
+		yield return new WaitForSeconds(randonTime);
+		randonTime=Random.Range(5,10.0f);
+		canSpawn=true;
 	}
 }
