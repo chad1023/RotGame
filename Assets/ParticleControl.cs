8f0c1d7039a472dee21537c77c0fedfd3b1b0d91
MingWei <lab430@LAB430deMacBook-Pro.local>
2017-06-06 13:03:48 +0800

    update audio rock destroy

---
+++ Assets/ParticleControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("Destroyitself", 1.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Destroyifself(){
+		JObjectPool._InstanceJObjectPool.Recovery (gameObject, Vector3.zero);
+	}
+}
